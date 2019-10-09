56f4538070e62a2b76b5d200cfdadf886caced27
Sung Jun Hong <47591360+SUNG-JUN-HONG@users.noreply.github.com>
2019-10-08 19:27:42 -0700

    Add files via upload

---
+++ FFTAnalysis/FFTAnalysis/Form1.Designer.cs
@@ -0,0 +1,377 @@
+﻿namespace FFTAnalysis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series11 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series12 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series13 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series14 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series15 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.trackBar4 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea5.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea5);
+            this.chart1.Location = new System.Drawing.Point(29, 22);
+            this.chart1.Name = "chart1";
+            series11.ChartArea = "ChartArea1";
+            series11.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series11.Name = "Waveform";
+            series12.ChartArea = "ChartArea1";
+            series12.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series12.Name = "Second Harmonic";
+            series13.ChartArea = "ChartArea1";
+            series13.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series13.Name = "Third Harmonic";
+            this.chart1.Series.Add(series11);
+            this.chart1.Series.Add(series12);
+            this.chart1.Series.Add(series13);
+            this.chart1.Size = new System.Drawing.Size(782, 266);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            chartArea6.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea6);
+            this.chart2.Location = new System.Drawing.Point(29, 313);
+            this.chart2.Name = "chart2";
+            series14.ChartArea = "ChartArea1";
+            series14.Name = "Frequency";
+            series15.ChartArea = "ChartArea1";
+            series15.Name = "Angle";
+            this.chart2.Series.Add(series14);
+            this.chart2.Series.Add(series15);
+            this.chart2.Size = new System.Drawing.Size(782, 278);
+            this.chart2.TabIndex = 1;
+            this.chart2.Text = "chart2";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(881, 756);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 48);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Load Signal";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(1063, 754);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 48);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "EXIT";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(371, 756);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(160, 49);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Perform FFT";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(88, 79);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(22, 21);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(224, 79);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(22, 21);
+            this.checkBox2.TabIndex = 6;
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(83, 140);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(69, 104);
+            this.trackBar1.TabIndex = 7;
+            this.trackBar1.Scroll += new System.EventHandler(this.TrackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(215, 140);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar2.Size = new System.Drawing.Size(69, 104);
+            this.trackBar2.TabIndex = 8;
+            this.trackBar2.Scroll += new System.EventHandler(this.TrackBar2_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(83, 390);
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar3.Size = new System.Drawing.Size(69, 104);
+            this.trackBar3.TabIndex = 9;
+            this.trackBar3.Scroll += new System.EventHandler(this.TrackBar3_Scroll);
+            // 
+            // trackBar4
+            // 
+            this.trackBar4.Location = new System.Drawing.Point(220, 394);
+            this.trackBar4.Name = "trackBar4";
+            this.trackBar4.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar4.Size = new System.Drawing.Size(69, 104);
+            this.trackBar4.TabIndex = 10;
+            this.trackBar4.Scroll += new System.EventHandler(this.TrackBar4_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(92, 117);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 20);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(228, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 20);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(92, 367);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 20);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(228, 367);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 20);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(79, 251);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 20);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "120Hz";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(211, 251);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 20);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "180Hz";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(79, 505);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(61, 20);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "120Hz";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(211, 505);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(61, 20);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "180Hz";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(90, 33);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(166, 32);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Maginitude";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(65, 315);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(224, 32);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "Phase (degree)";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.trackBar2);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.trackBar3);
+            this.panel1.Controls.Add(this.trackBar4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.checkBox2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.trackBar1);
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Location = new System.Drawing.Point(858, 125);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(357, 609);
+            this.panel1.TabIndex = 21;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.chart2);
+            this.panel2.Controls.Add(this.chart1);
+            this.panel2.Location = new System.Drawing.Point(12, 125);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(840, 609);
+            this.panel2.TabIndex = 22;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1246, 816);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Name = "Form1";
+            this.Text = "FFT Analysis";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.TrackBar trackBar4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ FFTAnalysis/FFTAnalysis/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+
+
+
+//References for FFT Analysis
+using MathNet.Numerics;
+using MathNet.Numerics.IntegralTransforms;
+using System;
+using System.Numerics;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+using MaterialSkin;
+using MaterialSkin.Controls;
+
+
+
+namespace FFTAnalysis
+{
+    public partial class Form1 : MaterialForm
+    {
+        //global variable declaration
+        static int numSamples = 1000;
+        static int sampleRate = 2000;
+        static int magSecond;
+        static int magThird;
+        static double PHSecond;
+        static double PHthird;
+        
+        Complex[] samples = new Complex[numSamples]; // creating a complex array
+
+        public Form1() // Consider it as the main method
+        {
+            InitializeComponent();
+        }
+
+        public void PlotWaveform(int secondHarm, int thirdHarm, double secondPH, double thridPH)
+        {
+            chart1.Series["Waveform"].Points.Clear();
+            chart1.Series["Second Harmonic"].Points.Clear();
+            chart1.Series["Third Harmonic"].Points.Clear();
+
+            //Generate fundamental, 2nd & 3rd Harmonic Waveform using MathNET library
+            double[] fundamental = Generate.Sinusoidal(numSamples, sampleRate, 60, 10.0);
+            double[] second = Generate.Sinusoidal(numSamples, sampleRate, 120, secondHarm, 0.0, secondPH);
+            double[] third = Generate.Sinusoidal(numSamples, sampleRate, 180, thirdHarm, 0.0, thridPH);
+
+            //Add waveforms together to get composite waveforms
+            for (int i = 0; i < numSamples; i++)
+            {
+                samples[i] = new Complex(fundamental[i] + second[i] + third[i], 0);
+            }
+
+            //Plot Composite Waveforms
+            //Each sample represents a time of 1/sampling rate
+            for (int i = 0; i < samples.Length / 5; i++)
+            {
+                double time = ((i + 1.0)) / numSamples / 2;
+
+                chart1.Series["Waveform"].LegendText = "Waveform";
+                chart1.Series["Waveform"].ChartType = SeriesChartType.Line;
+
+                chart1.ChartAreas["ChartArea1"].AxisX.TitleFont = new Font("Arial", 12.0f);
+                chart1.ChartAreas["ChartArea1"].AxisX.Title = "Seconds";
+
+                if (checkBox1.Checked)
+                {
+                    chart1.Series["Second Harmonic"].Points.AddXY(time, second[i]);
+                }
+
+                if (checkBox2.Checked)
+                {
+                    chart1.Series["Third Harmonic"].Points.AddXY(time, third[i]);
+                }
+
+                chart1.Series["Waveform"].Points.AddXY(time, samples[i].Real);
+
+
+            }
+
+
+        }
+
+        public void PlotFFT()
+        {
+            chart2.Series["Frequency"].Points.Clear();
+            Fourier.Forward(samples, FourierOptions.NoScaling); 
+            // "Forward" Fourier converts time => Frequency
+
+            //Plot the frequency spectrum. ("bidirectional bandwiddth"
+            //You only need the bottom of 1/2 of the samples
+            for (int i = 1; i < samples.Length / 2; i++)
+            {
+                chart2.ChartAreas["ChartArea1"].AxisX.Title = "HZ";
+                chart2.ChartAreas["ChartArea1"].AxisX.TitleFont = new Font("Arial", 12.0f);
+                chart2.ChartAreas["ChartArea1"].AxisX.MinorTickMark.Enabled = true;
+
+                //Get the maginitude of each FFT sample:
+                //= abs[sqrt(r^2 + i^2)]
+                double mag = (2.0 / numSamples) * (Math.Abs(Math.Sqrt(Math.Pow(samples[i].Real, 2) + Math.Pow(samples[i].Imaginary, 2))));
+
+                //Deteremine how many HZ represetned by each sample
+                double hzPerSample = sampleRate / numSamples;
+
+                chart2.Series["Frequency"].Points.AddXY(hzPerSample * i, mag);
+
+            }
+
+
+
+        }
+
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            //EXIT Button
+            Application.Exit();
+        }
+
+        private void TrackBar1_Scroll(object sender, EventArgs e)
+        {
+            //120HZ Magnitude
+            trackBar1.Enabled = true;
+            magSecond = trackBar1.Value;
+            label1.Text = magSecond.ToString("F0");
+            PlotWaveform(magSecond, magThird, PHSecond, PHthird);
+            PlotFFT();
+        }
+
+
+        // Event Handlers
+
+        private void TrackBar2_Scroll(object sender, EventArgs e)
+        {
+            //180HZ Magnitude
+            trackBar2.Enabled = true;
+            magThird = trackBar2.Value;
+            label2.Text = magThird.ToString("F0");
+            PlotWaveform(magSecond, magThird, PHSecond, PHthird);
+            PlotFFT();
+        }
+
+        private void TrackBar3_Scroll(object sender, EventArgs e)
+        {
+            //120 HZ Phase
+            //Value goes from 0 to 10, corresponding to 0 to 360 degrees, or 2*PI radians
+            //First Convert value to radians, where 10 = 360 degree or 2 PI radians
+            //trackBar3.Enabled = true;
+            PHSecond = 2.0*Math.PI*((trackBar3.Value) / (double)trackBar3.Maximum);
+            double angle = PHSecond * 360.0/(2*Math.PI);
+            label3.Text = angle.ToString("F1");
+            PlotWaveform(magSecond, magThird, PHSecond, PHthird);
+            PlotFFT();
+        }
+
+        private void TrackBar4_Scroll(object sender, EventArgs e)
+        {
+            //180 HZ Phase
+            //trackBar4.Enabled = true;
+            PHthird = 2.0*Math.PI*((trackBar4.Value) / (double)trackBar4.Maximum);
+            double angle = PHthird * 360.0/(2*Math.PI);
+            label4.Text = angle.ToString("F1");
+            PlotWaveform(magSecond, magThird, PHSecond, PHthird);
+            PlotFFT();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //Load Button
+            PlotWaveform(0, 0, 0, 0);
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            //FFT Button
+            PlotFFT();
+        }
+
+    }
+}
